078360112d058bfe2ca255cd8b199b1c19f7bcd6
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-04-30 22:04:25 -0600

    bug fix

--- src/app/auth/layouts/auth-layout/auth-layout.ts
+++ src/app/auth/layouts/auth-layout/auth-layout.ts
@@ -1,23 +1,26 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet],
   templateUrl: './auth-layout.html',
 })
 export class AuthLayout {
 
   router = inject(Router)
 
-  pageTitle$ = this.router.events.pipe(
-    filter((event) => event instanceof NavigationEnd),
-    map((event) => event.url),
-    map(
-      (url) =>
-        url.includes('login') ? 'Iniciar Sesión' : 'Registrarse'
+
+  pageTitle = toSignal(
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map((event) => event.url),
+      map(
+        (url) =>
+          url.includes('login') ? 'Iniciar Sesión' : 'Registrarse'
+      )
     )
   );
 }
